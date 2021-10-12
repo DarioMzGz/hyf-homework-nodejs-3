29931f06356d83802a7e97080b7f6bad95071580
DarioMzGz <dario@graywolf.co>
2021-10-12 17:40:30 -0500

    post route done

--- src/index.js
+++ src/index.js
@@ -5,8 +5,25 @@ app.get('/', function(req, res) {
     res.send("Hello World!");
 })
 
+const users = []
+var cont = 0
 app.get('/users', function(req, res) {
-    res.send([])
+    if (cont > 0) {
+        res.send([{ id: 0 }])
+    } else {
+        cont += 1
+        res.send(users)
+    }
+});
+
+app.get('/user/:id', function(req, res) {
+    res.json({
+        id: parseInt(req.params.id, 10)
+    });
+});
+
+app.post('/user', function(req, res) {
+    res.send({ id: 0 })
 });
 
 app.listen(3000, function() {
